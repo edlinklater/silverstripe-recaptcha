c168c2fc55aa1d3b47d27e7bbde533c18e307d7a
Oly Su <oly@silverstripe.com>
2015-07-16 22:07:44 +1200

    NOSCRIPT: Updated the non-JS fallback <noscript> html, so the textarea appears

--- code/RecaptchaField.php
+++ code/RecaptchaField.php
@@ -115,7 +115,7 @@ class RecaptchaField extends FormField {
 		// noscript fallback
 		$html .= <<<EOF
 <noscript>
-  <div style="width: 302px; height: 422px;">
+  <div style="width: 302px; height: 462px;">
     <div style="width: 302px; height: 422px; position: relative;">
       <div style="width: 302px; height: 422px; position: absolute;">
         <iframe src="{$iframeURL}"
@@ -123,17 +123,18 @@ class RecaptchaField extends FormField {
                 style="width: 302px; height:422px; border-style: none;">
         </iframe>
       </div>
-      <div style="width: 300px; height: 60px; border-style: none;
-                  bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px;
-                  background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px;">
-        <textarea id="g-recaptcha-response" name="g-recaptcha-response"
-                  class="g-recaptcha-response"
-                  style="width: 250px; height: 40px; border: 1px solid #c1c1c1;
-                         margin: 10px 25px; padding: 0px; resize: none;" >
-        </textarea>
-      </div>
+    </div>
+    <div style="border-style: none; bottom: 12px; left: 25px; margin: 0px; padding: 0px;
+                right: 25px; background: #f9f9f9; border: 1px solid #c1c1c1;
+                border-radius: 3px; height: 60px; width: 300px;">
+      <textarea id="g-recaptcha-response" name="g-recaptcha-response"
+                class="g-recaptcha-response"
+                style="width: 250px; height: 40px; border: 1px solid #c1c1c1;
+                margin: 10px 25px; padding: 0px; resize: none;">
+      </textarea>
     </div>
   </div>
+  <br>
 </noscript>
 EOF;
 
